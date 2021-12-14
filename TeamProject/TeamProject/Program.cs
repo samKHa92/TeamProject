630a8da9de9f4e72e1889a4477932aa17a58238e
burduliluka7 <63206890+burduliluka7@users.noreply.github.com>
2021-12-14 21:10:38 +0400

    Add files via upload

---
+++ TeamProject/TeamProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
